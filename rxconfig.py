46b7ab22aab6a3edf220fe49d313168bc7374b01
JohanTesla <johanmga@gmail.com>
2024-02-28 11:03:26 -0500

    cambio cositas del cors allowed origins

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="honguini_productions",
+     cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://honguiniproductions.com"
+    ]
 )
