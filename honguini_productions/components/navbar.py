bfa4d22f8ac972e1767e1370ee696da1463e7df6
JohanTesla <johanmga@gmail.com>
2024-03-08 16:01:17 -0500

    quito elementos de links en la navbar para hacerla responsive

--- honguini_productions/components/navbar.py
+++ honguini_productions/components/navbar.py
@@ -5,31 +5,32 @@ import honguini_productions.styles.styles as styles
 
 def navbar()-> rx.Component:
     return rx.hstack(
-        rx.hstack(
-            rx.image(src="/images/Honguini-01.png", width="2em"),
-            rx.link(
-                rx.box(
-                rx.text("Honguini", as_="span", color=Color.ORANGE.value),
-                rx.text("Productions", as_="span", color=Color.ORANGE_LIGHT.value),
-                style=styles.navbar_title_style
-                ),
-            href="https://honguiniproductions.com"
+        
+        rx.image(src="/images/Honguini-01.png", width="2em"),
+        rx.link(
+            rx.box(
+            rx.text("Honguini", as_="span", color=Color.ORANGE.value),
+            rx.text("Productions", as_="span", color=Color.ORANGE_LIGHT.value),
+            style=styles.navbar_title_style
             ),
+            href="https://honguiniproductions.com"
         ),
+        
         rx.spacer(),
-        rx.link(
-            "Inicio",
-            href="/",
-        ),
-        rx.link(
-            "Productos",
-            href="/productos",
-        ),
+        #rx.link(
+        #    "Inicio",
+        #    href="/",
+        #),
+        #rx.link(
+        #    "Productos",
+        #    href="/productos",
+        #),
 
-        rx.link(
-            "Contáctanos",
-            href="mailto:info@honguiniproductions.com",
-        ),
+        #rx.link(
+        #    "Contáctanos",
+        #    href="mailto:info@honguiniproductions.com",
+        #),
+        
         position="sticky",
         bg=Color.CONTENT.value,
         padding_x=Size.BIG.value,
